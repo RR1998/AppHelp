71150ebd963c55ea1fc1591397b3874f3d5c9f62
Roberto Rubio <Roberto_Rubio98@outlook.com>
2020-01-23 13:40:24 -0600

    :lipstick: HD2020-Login Added profile dimensions

---
+++ app/src/main/java/com/example/finalproject/useroverview/UserOverviewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.finalproject.useroverview
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserOverviewActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
