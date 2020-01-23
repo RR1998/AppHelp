b72299ec8654afcaf9a8d72dc6a621d21981df3c
Roberto Rubio <Roberto_Rubio98@outlook.com>
2020-01-23 16:43:40 -0600

    :lipstick: HD2020-UserOverview Add setContentView to onCreate

--- app/src/main/java/com/example/finalproject/useroverview/UserOverviewActivity.kt
+++ app/src/main/java/com/example/finalproject/useroverview/UserOverviewActivity.kt
@@ -3,9 +3,11 @@ package com.example.finalproject.useroverview
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.finalproject.R
 
 class UserOverviewActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_overview)
     }
 }
