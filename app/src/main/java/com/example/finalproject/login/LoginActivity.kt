c6f931b724777493f152d88b1a53f98e634638f5
Roberto Rubio <Roberto_Rubio98@outlook.com>
2020-01-26 14:28:56 -0600

    :sparkles: HD2020-Login Add LoginViewModel injection

--- app/src/main/java/com/example/finalproject/login/LoginActivity.kt
+++ app/src/main/java/com/example/finalproject/login/LoginActivity.kt
@@ -1,18 +1,21 @@
 package com.example.finalproject.login
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.finalproject.R
 import com.example.finalproject.databinding.ActivityLoginBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LoginActivity:AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityLoginBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private val loginViewModelInstance: LoginViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.loginActivityViewModel = loginViewModelInstance
     }
 }
