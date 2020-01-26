ecf2c4804046d73197c504b5f6fea18f88927ba6
Roberto Rubio <Roberto_Rubio98@outlook.com>
2020-01-26 14:43:19 -0600

    :recycle: HD2020-UserOverView Reformat code

--- app/src/main/java/com/example/finalproject/useroverview/UserOverviewActivity.kt
+++ app/src/main/java/com/example/finalproject/useroverview/UserOverviewActivity.kt
@@ -1,23 +1,22 @@
 package com.example.finalproject.useroverview
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.finalproject.R
 import com.example.finalproject.databinding.ActivityUserOverviewBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class UserOverviewActivity: AppCompatActivity() {
+class UserOverviewActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUserOverviewBinding
 
     private val userOverViewViewModelInstance: UserOverViewViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        
+
         super.onCreate(savedInstanceState)
-        binding =DataBindingUtil.setContentView(this, R.layout.activity_user_overview)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_overview)
         binding.userOverViewViewModel = userOverViewViewModelInstance
     }
 }
