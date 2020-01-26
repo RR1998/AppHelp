104aeba03f3d944784790c39e9ac418002704e2a
Roberto Rubio <Roberto_Rubio98@outlook.com>
2020-01-26 12:25:20 -0600

    :sparkles: HD2020-Login Add binding to LoginActivity

--- app/src/main/java/com/example/finalproject/login/LoginActivity.kt
+++ app/src/main/java/com/example/finalproject/login/LoginActivity.kt
@@ -3,9 +3,16 @@ package com.example.finalproject.login
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.finalproject.R
+import com.example.finalproject.databinding.ActivityLoginBinding
 
 class LoginActivity:AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
     }
 }
