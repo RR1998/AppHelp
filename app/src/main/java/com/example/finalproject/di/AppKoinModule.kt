23342b7bcf2c3f7289896653477631e3feec1a0f
Roberto Rubio <Roberto_Rubio98@outlook.com>
2020-01-26 14:36:58 -0600

    :sparkles: HD2020-UserOverView Add UserOverViewModel injection method

--- app/src/main/java/com/example/finalproject/di/AppKoinModule.kt
+++ app/src/main/java/com/example/finalproject/di/AppKoinModule.kt
@@ -1,9 +1,12 @@
 package com.example.finalproject.di
 
+import com.example.finalproject.useroverview.UserOverViewViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-
+    
+    viewModel { UserOverViewViewModel() }
 }
 
 val coreModule = module {
