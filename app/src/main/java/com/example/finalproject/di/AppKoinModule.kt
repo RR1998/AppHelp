b897cd49cdf2f884c22fbddceb334223549b497c
Roberto Rubio <Roberto_Rubio98@outlook.com>
2020-01-26 14:55:48 -0600

    :sparkles: HD2020-Areas Add NewRequestViewModel injection method

---
+++ app/src/main/java/com/example/finalproject/di/AppKoinModule.kt
@@ -0,0 +1,18 @@
+package com.example.finalproject.di
+
+import com.example.finalproject.newrequest.NewRequestViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+
+    viewModel { NewRequestViewModel() }
+}
+
+val coreModule = module {
+
+}
+
+val dataModule = module {
+
+}
