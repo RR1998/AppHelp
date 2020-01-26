306d5cb256614c8b54fc09dd8fe99acf67c6e316
Roberto Rubio <Roberto_Rubio98@outlook.com>
2020-01-26 11:57:24 -0600

    :sparkles: HD2020-UserOverView Add binding

--- app/src/main/java/com/example/finalproject/useroverview/UserOverviewActivity.kt
+++ app/src/main/java/com/example/finalproject/useroverview/UserOverviewActivity.kt
@@ -3,11 +3,16 @@ package com.example.finalproject.useroverview
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.finalproject.R
+import com.example.finalproject.databinding.ActivityUserOverviewBinding
 
 class UserOverviewActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityUserOverviewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_overview)
+        binding =DataBindingUtil.setContentView(this, R.layout.activity_user_overview)
     }
 }
