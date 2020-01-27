298e634ced6c05d59358e01d321cba929bc45043
jnefta91 <e.josem91@gmail.com>
2020-01-27 11:59:24 -0600

    :sparkles: update activity for getting viewmodel instance

--- app/src/main/java/com/example/finalproject/dashboardrequest/DashboardRequestActivity.kt
+++ app/src/main/java/com/example/finalproject/dashboardrequest/DashboardRequestActivity.kt
@@ -1,12 +1,19 @@
-package com.example.finalproject
+package com.example.finalproject.dashboardrequest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.finalproject.R
+import com.example.finalproject.databinding.ActivityDashboardRequestBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DashboardRequestActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityDashboardRequestBinding
+    private  val dashboardViewModel: DashboardViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_request)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_dashboard_request)
+        binding.dashboardViewModel = dashboardViewModel
     }
 }
